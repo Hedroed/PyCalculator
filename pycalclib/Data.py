f8b89c8df7c219abc8cddff356c748f57bf3c340
Rydin Nathan <nathan.rydin@orange.com>
2018-05-12 17:56:55 +0200

    Create data object class

---
+++ pycalclib/Data.py
@@ -0,0 +1,11 @@
+
+class Data():
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+
+class Variable():
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
