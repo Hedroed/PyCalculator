8a80c112f0598c69e038d05e4bdc1e01c80be068
Mathieu LUX <mathieu.lux@orange.com>
2018-05-13 21:18:34 +0200

    Add Base64

---
+++ pycalclib/type/Base64.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from .BaseType import BaseType
+# import registerType de je ne sais où ?
+from base64 import b64encode, b64decode
+import re
+
+
+class Base64(BaseType):
+    '''Base64 type
+        Base64 is a group of similar binary-to-text encoding schemes
+        that represent binary data in an ASCII string format by
+        translating it into a radix-64 representation.
+    '''
+    name = 'base64'
+
+    def format(self, value):
+        '''Format string to Base64'''
+        return b64decode(value)
+        # toBytes et format, type de retour de format = type d'entrée de format ?
+
+    def detect(self, value):
+        '''Is value a base64 string ?
+            A base64 string is composed of uppercase and lowercase letters,
+            digits (O to 9), +, / and =.
+        '''
+        return re.match('^\s*[A-Za-z0-9+/]*={0,2}\s*$', value) is not None
+
+    def fromBytes(self, _bytes):
+        '''Convert bytes to base64'''
+        return b64encode(_bytes)
+
+    def toBytes(self, _bytes):
+        '''Convert base64 to bytes'''
+        return _bytes
+
+    def toString(self, value):
+        '''Return value (base64) as string'''
+        return value.decode()
+
+
+# Register the type
+# registerType(Base64())
