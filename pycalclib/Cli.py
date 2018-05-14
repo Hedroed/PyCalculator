6494761ecd7fd14e2f27870b396b20c7605bc3bb
Rydin Nathan <nathan.rydin@gmail.com>
2018-05-14 21:17:33 +0200

    Work on cli

--- pycalclib/Cli.py
+++ pycalclib/Cli.py
@@ -1,5 +1,43 @@
 """
 For CLI usage
-
-TODO
 """
+import argparse
+
+from .Interpretor import Interpretor
+
+DESCRIPTION = "PyCalc"
+
+
+def parse_args():
+    """Arguments parsing."""
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+
+    parser.add_argument('-c',
+                        '--cmd',
+                        type=str,
+                        help='a command to interpret put result in stdout')
+
+    parser.add_argument('-f',
+                        '--file',
+                        type=str,
+                        help='a specific path to a save file.')
+
+    parser.add_argument('-v',
+                        '--verbose',
+                        help='verbose mode',
+                        action='count',
+                        default=0
+                        )
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    parse = parse_args()
+
+    it = Interpretor()
+
+    if parse.cmd:
+        print(it.interpret(parse.cmd))
