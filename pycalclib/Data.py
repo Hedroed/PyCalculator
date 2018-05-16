dfafa440f6cfd847928335500af8b924250dea26
Rydin Nathan <nathan.rydin@gmail.com>
2018-05-16 19:15:37 +0200

    Implement equality and representation for data class

--- pycalclib/Data.py
+++ pycalclib/Data.py
@@ -4,8 +4,25 @@ class Data():
         self.type = type
         self.value = value
 
+    def __eq__(self, other):
+        return (self.type == other.type and
+                self.value == other.value)
+
+    def __repr__(self):
+        typeName = "None"
+        if self.type:
+            typeName = self.type.name
+
+        return "Data(%s, %s)" % (typeName, self.value)
+
 
 class Variable():
     def __init__(self, name, data):
         self.name = name
         self.data = data
+
+    def __eq__(self, other):
+        return self.data == other.data
+
+    def __repr__(self):
+        return "Variable(%s, %s)" % (self.name, self.data)
